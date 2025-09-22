80fd882d32e0a674837230fb76be9be1aad1f19c
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-22 12:28:56 -0500

    feat(main): integrate layout component and add i18n support.

--- src/app.vue
+++ src/app.vue
@@ -1,9 +1,12 @@
 <script setup>
+import {useI18n} from "vue-i18n";
+import Layout from "./shared/presentation/components/layout.vue";
 
+const { t } = useI18n()
 </script>
 
 <template>
-
+  <layout/>
 </template>
 
 <style scoped>
