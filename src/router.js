88037e3c5a90584dc58e1011c59fd4cc2a20e743
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-22 11:59:31 -0500

    feat(in-app-navigation): add router configuration.

---
+++ src/router.js
@@ -0,0 +1,17 @@
+import Home from "./shared/presentation/views/home.vue";
+import {createRouter, createWebHistory} from "vue-router";
+
+const about = () => import('./shared/presentation/views/about.vue');
+
+const routes = [
+    { path: '/home',    name: 'home',   component: Home,    meta: { title: 'Home' } },
+    { path: '/about',   name: 'about',  component: about,   meta: { title: 'About' } },
+    { path: '/',        redirect: '/home'}
+];
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: routes
+});
+
+export default router;
