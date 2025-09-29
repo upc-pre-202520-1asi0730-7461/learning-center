88b0373c49a5ed0fc50da078a314b101fe4cb8c6
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-29 11:24:59 -0500

    feat(publishing): create publishing store for managing categories and tutorials.

---
+++ src/publishing/application/publishing.store.js
@@ -0,0 +1,75 @@
+import {PublishingApi} from "../infrastructure/publishing-api.js";
+import {defineStore} from "pinia";
+import {computed, ref} from "vue";
+import {CategoryAssembler} from "../infrastructure/category.assembler.js";
+import {TutorialAssembler} from "../infrastructure/tutorial.assembler.js";
+
+const publishingApi = new PublishingApi();
+
+const usePublishingStore = defineStore('publishing', () => {
+    // State
+    const categories  = ref([]);
+    const tutorials = ref([]);
+    const errors = ref([]);
+    const categoriesLoaded = ref(false);
+    const tutorialsLoaded = ref(false);
+    // Properties
+    const categoriesCount = computed(() => {
+        return categoriesLoaded ? categories.value.length : 0;
+    });
+    const tutorialsCount = computed(() => {
+        return tutorialsLoaded ? tutorials.value.length : 0;
+    });
+    // Actions
+    function fetchCategories() {
+        return publishingApi.getCategories().then(response => {
+            categories.value = CategoryAssembler.toEntitiesFromResponse(response);
+            categoriesLoaded.value = true;
+            console.log(categoriesLoaded.value);
+            console.log(categories.value);
+        }).catch(error => {
+            errors.value.push(error);
+        });
+    }
+
+    function fetchTutorials() {
+        return publishingApi.getTutorials().then(response => {
+            tutorials.value = TutorialAssembler.toEntitiesFromResponse(response);
+            tutorialsLoaded.value = true;
+        }).catch(error => {
+            errors.value.push(error);
+        });
+    }
+
+    function addCategory(category) {
+        publishingApi.createCategory(category).then(response => {
+            const resource = response.data;
+            const newCategory = CategoryAssembler.toEntityFromResource(resource);
+            categories.value.push(newCategory);
+        }).catch(error => {
+            errors.value.push(error);
+        });
+    }
+
+    function updateCategory(category) {
+        publishingApi.updateCategory(category).then(response => {
+            const resource = response.data;
+            const updatedCategory = CategoryAssembler.toEntityFromResource(resource);
+            const index = categories.value.findIndex(c => c["id"] === updatedCategory.id);
+            if (index !== -1) categories.value[index] = updatedCategory;
+        }).catch(error => {
+            errors.value.push(error);
+        });
+    }
+
+    function deleteCategory(category) {
+        publishingApi.deleteCategory(category.id).then(response => {
+            const index = categories.value.findIndex(c => c["id"] === category.id);
+            if (index !== -1) categories.value.splice(index, 1);
+        }).catch(error => {
+            errors.value.push(error);
+        });
+    }
+
+
+})
